5f065f205306472bd215cf2b559ce47c84f98c88
Larissa Albuquerque <larissaamca@gmail.com>
2026-07-27 11:15:56 -0300

    feat: add two additional Credly certification badges to the Certificados section

--- src/components/sections/Certificados.tsx
+++ src/components/sections/Certificados.tsx
@@ -41,6 +41,18 @@ export function Certificados({ t, activeSection }: CertificadosProps) {
           data-share-badge-id="6ed212c3-515a-4bf8-9bd9-509b6b177e51" 
           data-share-badge-host="https://www.credly.com"
         ></div>
+        <div 
+          data-iframe-width="150" 
+          data-iframe-height="270" 
+          data-share-badge-id="b61c0920-a6b7-4afd-806b-dfde84bbd5c9" 
+          data-share-badge-host="https://www.credly.com"
+        ></div>
+        <div 
+          data-iframe-width="150" 
+          data-iframe-height="270" 
+          data-share-badge-id="aa479d0c-7463-46b0-885e-92cc11ec20f6" 
+          data-share-badge-host="https://www.credly.com"
+        ></div>
       </div>
     </div>
   );
